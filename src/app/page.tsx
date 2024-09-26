a38f3f775d2d900cc179a97a3b576692b7761e77
geekchiq <chinnydevacct@gmail.com>
2024-09-26 14:39:03 +0800

    fix height for other screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Chat from './components/Chat'
 
 export default function Home() {
   return (
-    <div className="h-screen grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen">
-      <main className="flex sm:w-full md:w-2/4 h-full bg-cover bg-crystal-full">
+    <div className="h-screen grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen mb-10">
+      <main className="flex sm:w-full md:w-2/4 h-full bg-cover bg-crystal-full mb-20">
         <Chat />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center font-mono">
